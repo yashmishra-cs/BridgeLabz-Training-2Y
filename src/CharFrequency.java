5e99638822b81310d5c3480d166d856e464240d3
Yash Mishra <yk2551499@gmail.com>
2026-08-18 21:53:36 +0530

    Practice Problems

---
+++ src/CharFrequency.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CharFrequency {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] f = new int[256];
+        for(int i=0;i<s.length();i++){
+            f[s.charAt(i)]++;
+        }
+        for(int i=0;i<256;i++){
+            if(f[i]>0){
+                System.out.println((char)i+" = "+f[i]);
+            }
+        }
+    }
+}
